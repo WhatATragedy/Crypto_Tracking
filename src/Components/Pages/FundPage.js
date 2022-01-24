5c19a164ec7b89a27a5dc1077c7b0ce6a214f3b6
alex53856 <alex53856>
2022-01-24 19:50:24 +0000

    Cleaning up imports

--- src/Components/Pages/FundPage.js
+++ src/Components/Pages/FundPage.js
@@ -1,5 +1,4 @@
-import { useState } from "react";
-import { useQueries, useQuery } from "react-query";
+import { useQuery } from "react-query";
 import FundCards from "../Cards/FundCards";
 
 
@@ -7,14 +6,6 @@ import FundCards from "../Cards/FundCards";
 
 
 export default function FundPage() {
-    const [funds, setFunds] = useState([
-        {
-        "name": "Arweave",
-        "funds": ['Coinbase', 'Binance'],
-        "logo": "https://upload.wikimedia.org/wikipedia/commons/c/c5/Bitcoin_logo.svg"
-        }
-    ]);
-    const [dataFetched, setDataFetched] = useState(true)
 
     const { data: allFunds, isFetched } = useQuery("allFunds", () => fetch("https://xgmoe3viil.execute-api.us-east-2.amazonaws.com/reference/get-all")
     .then((response) => response.json()), { initialData: [] });
